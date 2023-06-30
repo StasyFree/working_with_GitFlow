622b070ee520bd0ddcdd417a7abb5fd8fcc8794e
Anastasia <aburlakova45@gmail.com>
2023-06-30 16:57:45 +0300

    пишем функцию capital_letters (без docstring)

--- str_func.py
+++ str_func.py
@@ -1,8 +1,3 @@
 def capital_letters(string_of_words):
-    """
-    Функция делает все символы заглавными в строке
-    :param string_of_words: строка
-    :return: строка заглавными буквами
-    """
     new_string = string_of_words.upper()
     return new_string
